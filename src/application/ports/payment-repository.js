735c5af16de2fa462202302d845a24c97e8f242e
wess-os <wes.lol25@gmail.com>
2025-08-08 15:05:07 -0300

    feat: define payment repository

---
+++ src/application/ports/payment-repository.js
@@ -0,0 +1,7 @@
+export class PaymentRepository {
+    create(payment) { throw new Error('Not implemented'); }
+
+    listAll() { throw new Error('Not implemented'); }
+
+    markAsPaid(id) { throw new Error('Not implemented'); }
+}
